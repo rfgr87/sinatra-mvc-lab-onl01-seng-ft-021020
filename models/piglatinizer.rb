65cbe673cf0576599aa4cb01bbe70214fc1b301a
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-24 21:25:33 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,8 +10,8 @@ class PigLatinizer
     words.map do |word|
       if (word =~ /yY/) == 0
         arr_words << (word[1..-1] + 'y' + 'ay')
-      elsif word.length == 1 && (word =~ /aAeEiIoOuU/) == 0 
-        arr_words << (word + 'way')
+      elsif word.length == 1 && first_vowel(word) == 0 
+        arr_words << (word[0] + 'way')
       else
         vowel_location = first_vowel(word)
         arr_words << (word[vowel_location..-1] + word[0..vowel_location-1] + 'ay')
