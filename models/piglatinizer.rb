af7eaa6f62d8bd6c3414a10ec5e7eefe16689279
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-24 21:30:39 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -12,8 +12,8 @@ class PigLatinizer
         arr_words << (word[1..-1] + 'y' + 'ay')
       elsif word.length == 1 && first_vowel(word) == 0 
         arr_words << (word[0] + 'way')
-      elsif first_vowel(word) == 0 
-        arr_word << (word + 'way')
+      # elsif first_vowel(word) == 0 
+      #   arr_word << (word + 'way')
       else
         vowel_location = first_vowel(word)
         arr_words << (word[vowel_location..-1] + word[0..vowel_location-1] + 'ay')
