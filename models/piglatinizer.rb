4ada4480f9355ec3a66c4a0a4cf12d1f43b98341
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-24 20:47:43 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,20 +1,35 @@
 class PigLatinizer
 
   def piglatinize(word)
-    if self.is_vowel?(word)
-      word.concat('way')
+    alphabet = ("A".."z").to_a
+    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    consonants = alphabet - vowels
+    unusable = ["i", "a", "an", "in", "on", "and"]
+    word.gsub!(/[^a-z0-9\s]/i, '')
+    word = word.split("")
+    # ["k", "i", "t", "t", "e", "n"]
+
+    if unusable.include?(word.join) || word.size <= 1
+      word.join
+    elsif vowels.include?(word[0]) && word.size > 1
+      word.join + "ay"
+    elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+      word = word.rotate.rotate.rotate << "ay"
+      word.join
+    elsif consonants.include?(word[0] && word[1])
+      word.rotate.rotate.join + "ay"
     else
-      word.concat(word.slice!(/^[^aeiouAEIOU]*/i || "")) + 'ay'
+      word.rotate.join + "ay"
     end
+
+end
+
+  def to_pig_latin(words)
+    result = words.split(" ").map do |word|
+      piglatinize(word)
+    end
+    result.join(" ")
   end
 
-  def to_pig_latin(text)
-    phrase.split.collect {|word| piglatinize(word)}.join(' ')
-  end
 
-  def is_vowel?(word)
-    vowels = 'aeiouAEIOU'
-    vowels.include?(word[0])
-  end
-
-end 
+end
